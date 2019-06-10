637a3b2324de2a164bedbb27652286c8d2acafe8
Carlos Meneses <kenshinmc23@gmail.com>
2019-06-10 00:56:58 -0400

    probando el login feature

--- out/test/resources/features/Login.feature
+++ out/test/resources/features/Login.feature
@@ -1,3 +1,6 @@
 Feature: Facebook login
   Scenario: As a user I must be able to access the application with the correct credentials
     When I access the "https://www.facebook.com/" page
+    And I filled the email field with "juancitopinto236@gmail.com"
+    And I filled the password field whit "123456.qwerty"
+    And I click on the button Entrar
--- src/test/java/stepdefinitions/LoginStepdefs.java
+++ src/test/java/stepdefinitions/LoginStepdefs.java
@@ -1,8 +1,12 @@
 package stepdefinitions;
 
 import browsermanager.BrowserManager;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 
 public class LoginStepdefs {
@@ -12,5 +16,32 @@ public class LoginStepdefs {
     @When("^I access the \"([^\"]*)\" page$")
     public void iAccessThePage(String url) {
         driver.navigate().to(url);
+    }
+
+    @And("^I filled the email field with \"([^\"]*)\"$")
+    public void iFilledTheEmailFieldWith(String email){
+        emailTextBox().sendKeys(email);
+    }
+
+    @And("^I filled the password field whit \"([^\"]*)\"$")
+    public void iFilledThePasswordFieldWhit(String pass){
+        passwordTextBox().sendKeys(pass);
+    }
+
+    @And("^I click on the button Entrar$")
+    public void iClickOnTheButtonEntrar() {
+        enterButton().click();
+    }
+
+    private WebElement emailTextBox(){
+        return driver.findElement(By.cssSelector("input#email"));
+    }
+
+    private WebElement passwordTextBox(){
+        return driver.findElement(By.cssSelector("input#pass"));
+    }
+
+    private WebElement enterButton(){
+        return driver.findElement(By.cssSelector("input[id='u_0_2']"));
     }
 }
